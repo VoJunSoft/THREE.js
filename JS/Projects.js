83f5c184cbfb2a0884d2d2c0b77c9825f9751e7a
Khaled Agbaria <61637325+VoJunSoft@users.noreply.github.com>
2022-07-14 16:37:04 +0300

    Update Projects.js

--- JS/Projects.js
+++ JS/Projects.js
@@ -1,31 +1,29 @@
  function displayData() {
      const GooglePlayLink = 'https://play.google.com/store/apps/dev?id=7276247598057063173'
      const data = [
-         { id: '0', title: 'Zaater', desc: 'Sellers App: Create your own store and start selling', img: '../Gallary/zaatar.png', link: GooglePlayLink },
-         { id: '1', title: 'Alhadath', desc: 'News App: Local news and business indexes for Google Play', img: '../Gallary/Alhadath.png', link: GooglePlayLink },
-         { id: '2', title: 'ZGshop', desc: 'Shopping App: Test project for clothing store', img: '../Gallary/clothing.png', link: GooglePlayLink },
-         { id: '3', title: 'Gama Cards', desc: 'Memory cards App: Just for fun', img: '../Gallary/Gama_ Cards.png', link: GooglePlayLink }
+         { id: '0', title: 'Zaater', desc: 'Sellers App: Create your own store and start selling', img: '../Gallary/zaatar.png', link: "https://play.google.com/store/apps/details?id=com.junglesoft.zaater" },
+         { id: '1', title: 'Alhadath', desc: 'News App: Local news and business indexes for Google Play', img: '../Gallary/Alhadath.png', link: "https://play.google.com/store/apps/details?id=com.junglesoft.alhadath" },
+         { id: '2', title: 'ZGshop', desc: 'Shopping App: Test project for clothing store', img: '../Gallary/clothing.png', link: "https://play.google.com/store/apps/details?id=com.junglesoft.zgshop" },
+         { id: '3', title: 'Gama Cards', desc: 'Memory cards App: Just for fun', img: '../Gallary/Gama_ Cards.png', link: "https://play.google.com/store/apps/details?id=com.junglesoft.gama" }
      ]
 
-     let footer = document.querySelector('.FooterProjects');
-     let recSpan;
-
+     let footer = document.querySelector('.FooterProjects')
      data.forEach(obj => {
-         recSpan = document.createElement('nav');
-         recSpan.setAttribute('id', `${obj.id}`);
+         recSpan = document.createElement('nav')
+         recSpan.setAttribute('id', `${obj.id}`)
 
          img = document.createElement('img')
-         img.setAttribute('id', `${obj.id}`);
+         img.setAttribute('id', `${obj.id}`)
          img.src = obj.img
-             //recSpan.innerHTML = `${obj.desc}`;
+
          recSpan.appendChild(img)
-         footer.appendChild(recSpan);
+         footer.appendChild(recSpan)
      });
 
      footer.addEventListener('click', e => {
-         let clickedID = e.target.id;
-         clickedID === '' ? null : alert(data[clickedID].title + ' : ' + data[clickedID].desc);
-     });
+         let clickedID = e.target.id
+         clickedID === '' ? null : window.open(data[clickedID].link, "_blank")
+     })
  }
 
  displayData()
